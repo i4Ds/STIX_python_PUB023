41b52edbaa2915a8d6a3c8609476baf535bd29af
root <hualin.xiao@gmail.com>
2019-03-27 10:21:03 +0100

    bug fix

--- tools/clipboard_hex_to_bin.py
+++ tools/clipboard_hex_to_bin.py
@@ -6,17 +6,21 @@
 import sys
 import binascii
 import Tkinter as tk
+import re
 def clipboard_hex_to_bin(out_filename):
     with open(out_filename,'wb') as fout :
         root = tk.Tk()
         root.withdraw()
-        data_hex=root.clipboard_get()
+        raw_hex=root.clipboard_get()
+        data_hex= re.sub(r"\s+", "", raw_hex)
+        print('header:%s'%data_hex[:10])
         data_binary = binascii.unhexlify(data_hex)
         fout.write(data_binary)
         print('data length:%d'%len(data_binary))
+        print('output:%s'%out_filename)
 def main():
     fname='clipboard.dat'
-    if len(sys.argv) > 2:
+    if len(sys.argv) >= 2:
         fname=sys.argv[1]
     clipboard_hex_to_bin(fname)
 if __name__ == '__main__':
