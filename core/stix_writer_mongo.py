d106f50bce8d59d1f2919bc828e8566e517d10b0
xiaohl <hualin.xiao@gmail.com>
2019-05-12 11:16:25 +0200

    add mongodb

--- core/stix_writer_mongo.py
+++ core/stix_writer_mongo.py
@@ -19,34 +19,41 @@ class stix_writer:
         self.packets=[]
         self.db=None
         self.packet_col=None
-        self.file_col=None
+        self.runs_col=None
         try :
             self.connect= pymongo.MongoClient('localhost', 27017)
             self.db = self.connect["stix"]
             self.packet_col=self.db['packets']
-            self.file_col=self.db['files']
+            self.runs_col=self.db['runs']
             
         except Exception as e:
             raise(e)
             print('can not connect to mongodb')
     def register_run(self,in_filename):
-        self.run_uuid=uuid.uuid1()
-        if self.file_col:
-           self.file_col.insert_one({'file':in_filename,
+        try:
+            self.last_run_id=self.runs_col.find().sort({'_id', -1}).limit(1)[0]['_id']
+        except TypeError:
+            self.last_run_id=-1
+        self.this_run_id=self.last_run_id+1
+        self.run_info={'file':in_filename,
                'date': datetime.datetime.now().isoformat(),
-               'run_uuid':self.run_uuid
-               })
-
+               }
 
     def write_header(self, header):
         pass
     def write(self,header, parameters, parameter_desc=dict()):
         packet={'header':header, 'parameter':parameters,
-                'file':self.filename}
+                'run_id':self.this_run_id}
         #'parameter_desc':parameter_desc
         self.packets.append(packet)
     def done(self):
+        start_time=self.packets[0]['header']['time']
+        end_time=self.packets[-1]['header']['time']
+        self.run_info['start']=start_time
+        self.run_info['end']=end_time
+        self.run_info['_id']=self.this_run_id
         if self.db:
+            self.runs_col.insert_one(self.run_info)
             self.packet_col.insert_many(self.packets)
     def write_all(self, data):
         pass
