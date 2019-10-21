769a7fbf65f7ab8c6ec9b9294bc342bb5ff0f168
hualin.xiao <hualin.xiao@gmail.com>
2019-10-21 15:23:32 +0200

    changed directory structure

--- setup.py
+++ setup.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python3
-
 import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-      name='StixParser',
-      version='1.6',
+      name='stix_parser',
+      version='1.3',
       description='STIX python data parser',
       author='Hualin Xiao',
       author_email='hualin.xiao@fhnw.ch',
       url='https://github.com/i4Ds/STIX-python-data-parser',
       packages=setuptools.find_packages(),
+      data_files=[('idb',['stix_parser/idb/idb.sqlite'])],
       install_requires=['numpy','PyQt5','PyQtChart','scipy','pymongo','python-dateutil','xmltodict'],
-      package_data={'idb': ['idb/idb.sqlite']},
-      scripts=['apps/stix-parser','apps/stix-parser-gui'],
+       entry_points={
+        'console_scripts': [
+            'stix-parser= stix_parser.apps.parser:main',
+            'stix-quicklook= stix_parser.apps.stix_quicklook:main',
+        ],
+        'gui_scripts': [
+            'stix-parser-gui= stix_parser.apps.stix_parser_gui:main',
+        ]
+    },
       python_requires='>=3.6'
 )
 
