c6fa6b289a70c8e2ff52aa30b9f405db61b5b6ce
hualin.xiao <hualin.xiao@gmail.com>
2019-11-24 18:53:02 +0100

    added modules

--- stix_parser/plugins/detector_test_to_root.py
+++ stix_parser/plugins/detector_test_to_root.py
@@ -9,7 +9,6 @@ GUI and execute this plugin in the plugin manager
 import os
 import sys
 from ROOT import TTree, TFile 
-from array import array
 
 sys.path.append(os.path.abspath('../../'))
 import numpy as np
@@ -28,6 +27,27 @@ DESCR = {
     54134: 'Dark current readouts',
     54130: 'threshold scan'
 }
+PIXEL_TO_ASIC= [
+     26,
+     15,
+    8,
+     1,
+     29,
+     18,
+    5,
+     0,
+     30,
+     21,
+     11,
+     3
+]
+def get_channel(ch):
+    try:
+        return PIXEL_TO_ASIC[ch]
+    except IndexError:
+        return -1
+
+
 def get_filename():
     root = Tk()
     root.filename = filedialog.asksaveasfilename(
@@ -36,6 +56,7 @@ def get_filename():
         filetypes=(("ROOT", "*.root"), ("all files", "*.*")))
     filename = root.filename
     root.withdraw()
+    return filename
 
 class Plugin:
     def __init__(self, packets=[], current_row=0):
@@ -49,41 +70,41 @@ class Plugin:
         fname=get_filename()
 
         f=TFile(fname,'recreate')
-        t=TTree('detector','detector test')
-        max_num=100000
+        t=TTree('stix','detector test')
 
-        spid= array('i', [0])
-        detectors =array('i', [0])
-        channels = array('i', [0])
-        ADC_mean =array('i', [0])
-        ADC_stdev = array('i', [0])
+        spid= np.empty((1), dtype='int32')
+        detector =np.empty((1), dtype='int32')
+        channel =np.empty((1), dtype='int32')
+        ADC_mean =np.empty((1), dtype='int32')
+        ADC_stdev =np.empty((1), dtype='int32') 
+        desc=np.empty((32), dtype='byte') 
 
         #des= array('c', 32*[''])
-        runs= array('i', [0])
+        run= np.empty((1), dtype='int32') 
 
         t.Branch('spid', spid,'spid/I')
-        t.Branch('detector', detectors,'detector/I')
-        t.Branch('channel', channels,'channels/I')
+        t.Branch('detector', detector,'detector/I')
+        t.Branch('channel', channel,'channel/I')
         t.Branch('mean', ADC_mean,'mean/I')
         t.Branch('std', ADC_stdev,'std/I')
-        t.Branch('run', runs,'run/I')
-        #t.Branch('desc', desc,'desc[32]/c')
+        t.Branch('run', run,'run/I')
+        t.Branch('desc', desc,'desc[32]/c')
 
 
         is_thr = False
         num_read = 0
+        current_run=0
         for pkt in self.packets:
             packet = sdt.Packet(pkt)
             if packet.SPID not in SPIDs and num_read == 0:
-                print('Not ADC readout report')
-                return
+                continue
             num_read += 1
             if packet.SPID not in SPIDs:
                 print('Not ADC readout report')
                 continue
             if packet.SPID == 54130:
                 is_thr = True
-            description = DESCR[packet.SPID]
+            desc= DESCR[packet.SPID]
 
             UTC = packet['header']['UTC']
             print('Analyzing packet # {}'.format(self.current_row))
@@ -98,16 +119,23 @@ class Plugin:
                 std = packet.get('NIX00104/NIX00107')[0]
             adc = packet.get(selector)[0]
             for k, d in enumerate(det):
-                spid[k]=packet.SPID
-                detectors[k]=d
-                channels[k]=cha[k]
-                ADC_mean[k]=adc[k]
+                spid[0]=packet.SPID
+                detector[0]=d
+                channel[0]=chan[k]
+                if get_channel(chan[k])==-1:
+                    continue
+
+                ADC_mean[0]=adc[k]
                 if not is_thr:
-                    ADC_stdev[k]=std[k]
-                runs[k]=current_run
+                    ADC_stdev[0]=std[k]
+                run[0]=current_run
                 t.Fill()
 
 
             if packet.seg_flag in [2, 3]:
                 current_run+=1
             self.current_row += 1
+
+        print('Number of packets:',num_read)
+        t.Write()
+        f.Close()
