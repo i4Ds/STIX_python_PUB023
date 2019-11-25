ef55a7ead53d63b82a5cef0614cbc53b032aa71d
hualin.xiao <hualin.xiao@gmail.com>
2019-11-25 23:08:52 +0100

    added all plugins

---
+++ stix_parser/plugins/TCTM_to_csv.py
@@ -0,0 +1,69 @@
+#plugin example
+import pprint
+
+ISTART = 0
+ISTOP = 0
+
+
+class Plugin:
+    """ don't modify here """
+
+    def __init__(self, packets=[], current_row=0):
+        self.packets = packets
+        self.current_row = current_row
+        print("Plugin  loaded ...")
+
+    def run(self, istart=ISTART, istop=ISTOP):
+        last_time = 0
+        num_TC = 0
+        total_size = 0
+        num_TM = 0
+        TC_size = 0
+        counter = {}
+        packets = self.packets
+        if istop > istart:
+            packets = self.packets[istart:istop]
+
+        for packet in packets:
+            header = packet['header']
+            if header['service_type'] == 20 and header[
+                    'service_subtype'] == 128:
+                continue
+            leng = header['raw_length']
+            spid = header['SPID']
+            extra = ''
+            extra2 = ''
+            if header['TMTC'] == 'TC':
+                if header['name'] == 'ZIX20128':
+                    continue
+                num_TC += 1
+                TC_size += header['raw_length']
+                leng = 0
+                extra2 = ';'
+            else:
+                if header['SPID'] in [54103, 54101]:
+                    continue
+                total_size += header['raw_length']
+                num_TM += 1
+                apid = header['apid']
+                if apid not in counter:
+                    counter[apid] = 0
+                counter[apid] += header['raw_length']
+                extra = ';'
+
+            #if spid == 54102:
+                #if header['SCET'] < last_time + 14:
+                #    continue
+                #last_time = header['SCET']
+
+            print('{}; {} {}({},{}) - {}; {} {}; {}'.format(header['UTC'], extra, header['TMTC'], int(header['service_type']),
+                                                  int(header['service_subtype']),
+                                                  header['descr'], extra2, leng, header['apid'],))
+
+        #print('Number of TC,', num_TC)
+        #print('TC size,', TC_size)
+        #print('TM Total size,', total_size)
+        #print('Number of TM,', num_TM)
+        #print('APID, length')
+        #for key, value in counter.items():
+        #    print('{}, {}'.format(key, value))
--- stix_parser/plugins/detector_test_to_root.py
+++ stix_parser/plugins/detector_test_to_root.py
@@ -1,14 +1,13 @@
 # -*- encoding: utf-8 -*-
 """ A  Qt parser GUI plugin used to plot ASIC on-demand readouts, threshold readouts and 
     dark current readouts. 
-
 In order to use it, you need to select a background report in the 
 GUI and execute this plugin in the plugin manager
 """
 
 import os
 import sys
-from ROOT import TTree, TFile 
+from ROOT import TTree, TFile
 
 sys.path.append(os.path.abspath('../../'))
 import numpy as np
@@ -27,20 +26,9 @@ DESCR = {
     54134: 'Dark current readouts',
     54130: 'threshold scan'
 }
-PIXEL_TO_ASIC= [
-     26,
-     15,
-    8,
-     1,
-     29,
-     18,
-    5,
-     0,
-     30,
-     21,
-     11,
-     3
-]
+PIXEL_TO_ASIC = [26, 15, 8, 1, 29, 18, 5, 0, 30, 21, 11, 3]
+
+
 def get_channel(ch):
     try:
         return PIXEL_TO_ASIC[ch]
@@ -58,6 +46,7 @@ def get_filename():
     root.withdraw()
     return filename
 
+
 class Plugin:
     def __init__(self, packets=[], current_row=0):
         self.packets = packets
@@ -67,33 +56,37 @@ class Plugin:
     def run(self):
         isub = 0
         fig = None
-        fname=get_filename()
+        fname = get_filename()
 
-        f=TFile(fname,'recreate')
-        t=TTree('stix','detector test')
+        f = TFile(fname, 'recreate')
+        t = TTree('stix', 'detector test')
 
-        spid= np.empty((1), dtype='int32')
-        detector =np.empty((1), dtype='int32')
-        channel =np.empty((1), dtype='int32')
-        ADC_mean =np.empty((1), dtype='int32')
-        ADC_stdev =np.empty((1), dtype='int32') 
-        desc=np.empty((32), dtype='byte') 
+        coarse_time = np.empty((1), dtype=np.uint32)
+        packet_id= np.empty((1), dtype=np.uint32)
+        spid = np.empty((1), dtype='int32')
+        detector = np.empty((1), dtype='int32')
+        channel = np.empty((1), dtype='int32')
+        ADC_mean = np.empty((1), dtype='int32')
+        ADC_stdev = np.empty((1), dtype='int32')
+        desc = np.empty((32), dtype='byte')
 
         #des= array('c', 32*[''])
-        run= np.empty((1), dtype='int32') 
+        run = np.empty((1), dtype='int32')
 
-        t.Branch('spid', spid,'spid/I')
-        t.Branch('detector', detector,'detector/I')
-        t.Branch('channel', channel,'channel/I')
-        t.Branch('mean', ADC_mean,'mean/I')
-        t.Branch('std', ADC_stdev,'std/I')
-        t.Branch('run', run,'run/I')
-        t.Branch('desc', desc,'desc[32]/c')
-
+        t.Branch('spid', spid, 'spid/I')
+        t.Branch('detector', detector, 'detector/I')
+        t.Branch('channel', channel, 'channel/I')
+        t.Branch('mean', ADC_mean, 'mean/I')
+        t.Branch('std', ADC_stdev, 'std/I')
+        t.Branch('run', run, 'run/I')
+        t.Branch('desc', desc, 'desc[32]/C')
+        t.Branch('coarse_time', coarse_time, 'coarse_time/I')
+        t.Branch('packet_id', packet_id, 'packet_id/I')
 
         is_thr = False
         num_read = 0
-        current_run=0
+        current_run = 0
+        packet_id[0]=0
         for pkt in self.packets:
             packet = sdt.Packet(pkt)
             if packet.SPID not in SPIDs and num_read == 0:
@@ -104,10 +97,11 @@ class Plugin:
                 continue
             if packet.SPID == 54130:
                 is_thr = True
-            desc= DESCR[packet.SPID]
+            desc = DESCR[packet.SPID]
 
-            UTC = packet['header']['UTC']
             print('Analyzing packet # {}'.format(self.current_row))
+            coarse_time[0]= packet.coarse_time
+            packet_id[0]+=1
             det = packet.get('NIX00104/NIX00100')[0]
             chan = packet.get('NIX00104/NIX00105')[0]
             adc = []
@@ -119,23 +113,22 @@ class Plugin:
                 std = packet.get('NIX00104/NIX00107')[0]
             adc = packet.get(selector)[0]
             for k, d in enumerate(det):
-                spid[0]=packet.SPID
-                detector[0]=d
-                channel[0]=chan[k]
-                if get_channel(chan[k])==-1:
+                spid[0] = packet.SPID
+                detector[0] = d
+                channel[0] = chan[k]
+                if get_channel(chan[k]) == -1:
                     continue
 
-                ADC_mean[0]=adc[k]
+                ADC_mean[0] = adc[k]
                 if not is_thr:
-                    ADC_stdev[0]=std[k]
-                run[0]=current_run
+                    ADC_stdev[0] = std[k]
+                run[0] = current_run
                 t.Fill()
 
-
             if packet.seg_flag in [2, 3]:
-                current_run+=1
+                current_run += 1
             self.current_row += 1
 
-        print('Number of packets:',num_read)
+        print('Number of packets:', num_read)
         t.Write()
         f.Close()
