0bb947582e0af9854f60ed169588503e70e9a260
xiaohl <dr.hualinxiao@gmail.com>
2020-08-21 10:24:18 +0200

    added flare importer

---
+++ stix/tools/import_flares.py
@@ -0,0 +1,31 @@
+import csv
+f=open('STIX_flare_list_commissioning.csv')
+data = csv.DictReader(f)
+import pymongo
+from dateutil import parser as dtparser
+connect = pymongo.MongoClient()
+db = connect["stix"]
+col = db['flares']
+
+def utc2unix(utc):
+    if isinstance(utc, str):
+        if not utc.endswith('Z'):
+            utc += 'Z'
+        try:
+            return dtparser.parse(utc).timestamp()
+        except:
+            return 0
+    elif isinstance(utc, int) or isinstance(utc, float):
+        return utc
+    else:
+        return 0
+    
+
+
+for i, l in enumerate(data):
+    l['_id']=i
+    l['start_unix_time']=utc2unix(l['date']+'T'+l['start'])
+    l['duration']=-(l['start_unix_time']-utc2unix(l['date']+'T'+l['end']))
+    l['hidden']=False
+    print(l)
+    col.insert_one(l)
