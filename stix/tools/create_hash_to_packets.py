bc5dbf97ff6f2bbb8f88e3c0fe8e7fb25daa3653
xiaohl <dr.hualinxiao@gmail.com>
2021-01-13 18:23:51 +0100

    create hash for packets

---
+++ stix/tools/create_hash_to_packets.py
@@ -0,0 +1,25 @@
+import pymongo
+import hashlib
+connect = pymongo.MongoClient()
+db = connect["stix"]
+packet_db= db['packets']
+cursor=packet_db.find()
+#cursor=packet_db.find({'_id':771287})
+total=cursor.count()
+per=int(total/100)
+i=0
+for pkt in cursor:
+    header_str=str(pkt['header'])
+    pkt['hash']=hashlib.shake_256(header_str).hexdigest(8)
+    packet_db.replace_one({'_id':pkt['_id']}, pkt)
+    i+=1
+
+    if i%per==0:
+        print(total/per)
+            
+
+
+    
+
+    
+    
